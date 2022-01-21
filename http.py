434aab96ddad51a18dc2a29b86bf9d127f58400c
Felix Malmsjö <felix@malmsjo.com>
2022-01-21 23:27:51 +0100

    Can't get the second line short enough by one character

--- http.py
+++ http.py
@@ -1,7 +1,5 @@
-(i:=[__import__(x)for x in["socket","os","threading"]],s:=lambda x,y=" ":x.split(y),
-u:=i[0].socket(),u.bind(("",80)),u.listen(5),h:=lambda c,R="\r\n":(d:=(l:=(
-lambda a,d:(((d,lambda a,b:(b,)),(b:=d+c.recv(2048).decode(),a))[R*2 in d][1](a
-,b))))(l,"")[0],u:=s(s(s(s(d,R*2)[0],R)[0])[1],"?")[0][1:],d:=(f:=open(("index"
-".html",u)[i[1].path.isfile(u)],"rb")).read(),f.close(),c.send((f"HTTP/1.1 200"
-f"{R}Content-Length:{len(d)}{R*2}").encode()+d),c.close()),(k:=(lambda a:(i[2
-].Thread(target=h,args=(u.accept()[0],)).start(),a(a))))(k))
+(R:=b"\r\n",s:=lambda x,y=None:x.split(y),i:=[__import__(x)for x in s("socket "
+"os _thread")],u:=i[0].socket(),u.bind(("",80)),u.listen(5),h:=lambda c:(d:=(l:=(lambda a,d:(((d,lambda a,b:(b,)),(b:=d+c.recv(2048),a))[R*2 in d][1](a,b)))
+)(l,b"")[0],u:=s(s(s(s(d,R*2)[0],R)[0])[1],b"?")[0][1:],d:=(f:=open(("i.htm",u)
+[i[1].path.isfile(u)],"rb")).read(),f.close(),c.send(b"HTTP/2 200"+R+b"Content"
+b"-Length:"+str(len(d)).encode()+R*2+d),c.close()),(k:=(lambda a:(i[2].start_new_thread(h,(u.accept()[0],)),a(a))))(k))
