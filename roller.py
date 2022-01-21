3667e2204c758e6b80b36b53f8207c059f5130c5
Felix Malmsjö <felix@malmsjo.com>
2022-01-21 22:53:08 +0100

    Add dice roller for fun

---
+++ roller.py
@@ -0,0 +1,5 @@
+(k:=(lambda a:(v:=lambda c,ge,d:[(x-c(ge)/d(ge))**2 for x in ge],b:=sum,l:=len,
+p:=input("[NdN?]: ").split("d"),f:=int(p[0]),h:=None,e:=__import__,srt:=sorted,
+g:=srt([e('random',h,h,['randint'],0).randint(1,int(p[1])) for _ in range(f)]),
+i:=(b(v(b,g,l))/l(g))**.5,s:=b(g[l(g)//2-1+f%2:l(g)//2+1])/(2-f%2),"kim&felix",
+m:=print(f'List={g}\nTot={b(g)}\nAvg={b(g)/l(g)}\nStd={i}\nMed={s}'),a(a))))(k)
