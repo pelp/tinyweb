7a07b77df1299f2bc6961a166b159d9a30464fbd
Felix Malmsjö <felix@malmsjo.com>
2022-01-21 22:31:51 +0100

    Almost done minimizing

--- http.py
+++ http.py
@@ -1,8 +1,7 @@
-import socket as u, os, threading as t
-R,s,_=("\r\n",lambda x,y=None:x.split(y),(u:=u.socket(),u.bind(("",3000)),u.listen(5)))
-h=lambda c:(d:=(l:=(lambda a,d:(((d,lambda a,b:(b,)),(b:=d+c.recv(2048).decode(),
-a))[R*2 in d][1](a,b))))(l,"")[0],url:=s(s(s(s(d,R*2)[0],R)[0])[1],"?")[0][1:],
-d:=(f:=open(("index.html",url)[os.path.isfile(url)],"rb")).read(),f.close(),
-c.send((f"HTTP/1.1 200 OK{R}Content-Length: {len(d)}{R*2}").encode()+d),
-c.close())
+import socket as u, os as o, threading as t
+s,_=(lambda x,y=None:x.split(y),(u:=u.socket(),u.bind(("",3000)),u.listen(10)))
+h=lambda c,R="\r\n":(d:=(l:=(lambda a,d:(((d,lambda a,b:(b,)),(b:=d+c.recv(2048
+).decode(),a))[R*2 in d][1](a,b))))(l,"")[0],u:=s(s(s(s(d,R*2)[0],R)[0])[1],"?"
+)[0][1:],d:=(f:=open(("index.html",u)[o.path.isfile(u)],"rb")).read(),f.close()
+,c.send((f"HTTP/1.1 200{R}Content-Length:{len(d)}{R*2}").encode()+d),c.close())
 (k:=(lambda a:(t.Thread(target=h,args=(u.accept()[0],)).start(),a(a))))(k)
