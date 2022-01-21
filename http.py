64feb93e7bf5b7d7f10176429b96cc723f7b69b5
Felix Malmsjö <felix@malmsjo.com>
2022-01-21 21:42:53 +0100

    Debug nested lambda functions and recursion hell

--- http.py
+++ http.py
@@ -1,24 +1,36 @@
-import socket as u, threading as t
+import socket as u
+import os
+import threading as t
 RN = "\r\n"
 RNB = b"\r\n"
 s=lambda x,y=None:x.split(y)
-def ch(c):
- data = ""
- while RN*2 not in data:
-  data += c.recv(2048).decode("utf-8")
- url = s(s(s(s(data,RN*2)[0],RN)[0])[1],"?")[0][1:]
- url = (url, "index.html")[url in ("", "index")]
- try:
-  with open(url, "rb") as f:
-   data = f.read()
-   c.send((f"HTTP/1.1 200 OK{RN}Content-Length: {len(data)}{RN*2}").encode("utf-8") + data)
- except Exception:
-  c.send(b"HTTP/1.1 418 I'm a teapot" + RNB*2)
- c.close()
+def non(a, b):
+    pass
+
+h=lambda c: (
+    l := (
+        lambda a,d: (
+            (
+                (b := d+c.recv(2048).decode("utf-8"), a, print(d)),
+                (d, non)
+            )[RN*2 not in b][1](a, b)
+        )
+    ),
+    d:=l(l, "")[0],
+    print("Done reading"),
+    url:=s(s(s(s(d, RN*2)[0], RN)[0])[1], "?")[0][1:],
+    exists:=os.path.isfile(url),
+    f:=open(("index.html",url)[exists], "rb"),
+    d:=f.read(),
+    c.send((f"HTTP/1.1 200 OK{RN}Content-Length: {len(d)}{RN*2}").encode("utf-8") + d),
+    f.close(),
+    c.close()
+)
+
+
 u = u.socket()
 u.bind(("0.0.0.0", 3000))
 u.listen(5)
 while True:
- c, _ = u.accept()
- t.Thread(target=ch, args=(c,)).start()
- #certifiedwomenfriendlyprograming #Loverofwomen #Bringbackväsensmetafysiken #Bringbackwindowsvista #MulleMeck=Slut???
+    t.Thread(target=h, args=(u.accept()[0],)).start()
+    # certifiedwomenfriendlyprograming #Loverofwomen #Bringbackväsensmetafysiken #Bringbackwindowsvista #MulleMeck=Slut???
