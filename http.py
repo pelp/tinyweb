3fc1cd6320112bdfd6601a4e80087595fa166d59
Felix Malmsjö <felix@malmsjo.com>
2022-01-21 22:45:41 +0100

    Moved imports to dunder imports

--- http.py
+++ http.py
@@ -1,7 +1,7 @@
-import socket as u, os as o, threading as t
-s,_=(lambda x,y=None:x.split(y),(u:=u.socket(),u.bind(("",3000)),u.listen(10)))
+i,s,_=(i:=[__import__(x)for x in["socket","os","threading"]],lambda x,y=" "
+:x.split(y),(u:=i[0].socket(),u.bind(("",80)),u.listen(5)))
 h=lambda c,R="\r\n":(d:=(l:=(lambda a,d:(((d,lambda a,b:(b,)),(b:=d+c.recv(2048
 ).decode(),a))[R*2 in d][1](a,b))))(l,"")[0],u:=s(s(s(s(d,R*2)[0],R)[0])[1],"?"
-)[0][1:],d:=(f:=open(("index.html",u)[o.path.isfile(u)],"rb")).read(),f.close()
+)[0][1:],d:=(f:=open(("index.html",u)[i[1].path.isfile(u)],"rb")).read(),f.close()
 ,c.send((f"HTTP/1.1 200{R}Content-Length:{len(d)}{R*2}").encode()+d),c.close())
-(k:=(lambda a:(t.Thread(target=h,args=(u.accept()[0],)).start(),a(a))))(k)
+(k:=(lambda a:(i[2].Thread(target=h,args=(u.accept()[0],)).start(),a(a))))(k)
