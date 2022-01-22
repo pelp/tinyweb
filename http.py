9c4d129a35801ebf454c7c4f335f74eb838e37db
Felix Malmsjö <felix@malmsjo.com>
2022-01-22 01:24:07 +0100

    Done.

--- http.py
+++ http.py
@@ -3,7 +3,7 @@
 c:(d:=(l:=(lambda a,d:(((d,lambda a,b:(b,)),(b:=d+c.recv(2048),a))[R*2 in d][1]
 (a,b))))(l,b"")[0],y:=s(s(s(s(d,R*2)[0],R)[0])[1],b"?"),u:=y[0][1:],"felix&kim"
 ,d:=(lambda x:((f:=open(("i.htm",u)[i[1].path.isfile(x)],"rb")).read(),f.close(
-))[0],lambda x:i[3].run(["python",x,b"".join(y[1:])],stdout=i[3].PIPE).stdout)[
-s(u,b".")[-1]==b"py"](u),c.send((b"HTTP/2 200\r\nContent-Type:text/html\r\nCon"
-b"tent-Length:"+str(len(d)).encode()+b"\r\n\r\n"+d)),c.close()),(k:=(lambda a:(
-i[2].start_new_thread(h,(u.accept()[0],)),a(a))))(k))
+))[0],lambda x:i[3].run(["py",x,b"".join(y[1:])],stdout=i[3].PIPE).stdout)[s(u,
+b".")[-1]==b"py"](u),c.send((b"HTTP/2 200\r\nServer:tw/v1\r\nContent-Type:text"
+b"/html;charset=utf-8\r\nContent-Length:"+str(len(d)).encode()+b"\r\n\r\n"+d)),
+c.close()),(k:=(lambda a:(i[2].start_new_thread(h,(u.accept()[0],)),a(a))))(k))
--- roller.py
+++ roller.py
@@ -1,5 +1,5 @@
-(k:=(lambda a:(v:=lambda c,ge,d:[(x-c(ge)/d(ge))**2 for x in ge],b:=sum,l:=len,
-p:=input("[NdN?]: ").split("d"),f:=int(p[0]),h:=None,e:=__import__,srt:=sorted,
-g:=srt([e('random',h,h,['randint'],0).randint(1,int(p[1])) for _ in range(f)]),
-i:=(b(v(b,g,l))/l(g))**.5,s:=b(g[l(g)//2-1+f%2:l(g)//2+1])/(2-f%2),"kim&felix",
-m:=print(f'List={g}\nTot={b(g)}\nAvg={b(g)/l(g)}\nStd={i}\nMed={s}'),a(a))))(k)
+(k:=(v:=lambda c,g,d:[(x-c(g)/d(g))**2 for x in g],b:=sum,l:=len,e:=__import__,
+p:=(e('sys').argv[1].split("ndn=")[1].split("d")),f:=int(p[0]),r:=sorted,g:=r([
+e('random').randint(1,int(p[1])) for _ in range(f)]),i:=(b(v(b,g,l))/l(g))**.5,
+s:=b(g[l(g)//2-1+f%2:l(g)//2+1])/(2-f%2),"kim&felix",m:=print('List:'+"♥".join(
+map(str,g))+f'<br>Total:{b(g)}<br>Avg.:{b(g)/l(g)}<br>Std:{i}<br>Median:{s}')))
